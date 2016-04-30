54fe2da764fe8f885545710d7a2462e22c450a9b
Connie Leung <cleung334@gmail.com>
2016-04-30 23:00:31 +0800

    Create factory component and display factory names in left sidebar.

--- app/factory/factory.component.ts
+++ app/factory/factory.component.ts
@@ -2,18 +2,20 @@ import {Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 
 import {FactoryService} from './service/factory.service';
+import {Factory} from './model/factory';
 
 @Component({
   selector: 'factory-list',
-  template: `
-    <h1>testing</h1>
-  `,
+  templateUrl: 'app/factory/template/factory.html' ,
+  directives: [MATERIAL_DIRECTIVES],
   providers: [FactoryService]
 })
 export class FactoryComponent {
 
+  factories: Array<Factory>;
+  
   //  factory
-  constructor() {
-
+  constructor(_factoryService: FactoryService) {
+    this.factories = _factoryService.getFactories();
   }
 }
