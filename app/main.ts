214797ee22231c0de25609a05cd2a1ac51011cc3
Connie Leung <cleung334@gmail.com>
2016-04-30 12:51:03 +0800

    initial commit
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+import {MATERIAL_PROVIDERS} from "ng2-material/all";
+
+
+bootstrap(AppComponent, [MATERIAL_PROVIDERS]);
