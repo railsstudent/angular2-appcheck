a8a79151d26136ca26fd70b10df1566a0e021b4f
Connie Leung <cleung334@gmail.com>
2016-05-03 00:10:27 +0800

    Add skeleton code of database list component

---
+++ app/factory-database/database-list.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit } from 'angular2/core';
+import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
+import {Router, RouteParams} from 'angular2/router';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: 'app/factory-database/template/database-list.html' ,
+  directives: [MATERIAL_DIRECTIVES],
+  providers: []
+})
+export class DatabaseListComponent  {
+
+
+}
