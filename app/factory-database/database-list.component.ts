7d048b7d0e81c7dae56e185a97380cb5d7cd3198
Connie Leung <cleung334@gmail.com>
2016-05-03 00:12:01 +0800

    Add skeleton code of database list service.

--- app/factory-database/database-list.component.ts
+++ app/factory-database/database-list.component.ts
@@ -3,11 +3,13 @@ import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {Router, RouteParams} from 'angular2/router';
 import * as _ from 'lodash';
 
+import {DatabaseListService} from './service/database.service';
+
 @Component({
   selector: 'app-detail',
   templateUrl: 'app/factory-database/template/database-list.html' ,
   directives: [MATERIAL_DIRECTIVES],
-  providers: []
+  providers: [DatabaseListService]
 })
 export class DatabaseListComponent  {
 
---
+++ app/factory-database/service/database.service.ts
@@ -0,0 +1,7 @@
+import {Injectable} from 'angular2/core';
+import * as _ from 'lodash';
+
+@Injectable()
+export class DatabaseListService {
+
+}
