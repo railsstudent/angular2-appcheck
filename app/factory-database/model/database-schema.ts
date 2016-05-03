1ea2a4e03ec5ae516fb7fc0ce2aafec0156ada93
Connie Leung <cleung334@gmail.com>
2016-05-04 01:51:55 +0800

    commit

--- app/factory-database/model/database-schema.ts
+++ app/factory-database/model/database-schema.ts
@@ -1,5 +1,15 @@
 
 export class DatabaseSchema {
 
-  
+  id: number;
+  dbId: number;
+  name: string;
+  memoryUsed: number;
+
+  constructor(id: number, dbId: number, name: string, memoryUsed: number) {
+    this.id = id;
+    this.dbId = dbId;
+    this.name = name;
+    this.memoryUsed = memoryUsed;
+  }
 }
