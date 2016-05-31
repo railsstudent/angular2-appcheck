404b3a03eb1f343db4040f160fefe91d94f79930
Connie <cleung334@gmail.com>
2016-05-31 22:43:47 +0800

    Add properties to virtual machine class

--- app/factory-virtualmachine/model/virtual-machine.ts
+++ app/factory-virtualmachine/model/virtual-machine.ts
@@ -1,5 +1,33 @@
 
-
 export class VirtualMachine {
+
+  id: number;
+  code: string;
+  ipAddress: string;
+  dns: string;
+  osPlatform: string;
+  ram:  number;
+  diskSpace: number;
+  manufacturer: string;
+  model: string;
+  yearOfService : number;
+  condition: string;
+
+  constructor (id: number, code: string, ipAddress: string, dns: string,
+    osPlatform: string, ram: number, diskSpace: number,
+    manufacturer: string, model: string, yearOfService: number,
+    condition: string) {
+       this.id = id;
+       this.code = code;
+       this.ipAddress = ipAddress;
+       this.dns = dns;
+       this.osPlatform = osPlatform;
+       this.ram = ram;
+       this.diskSpace = diskSpace;
+       this.manufacturer = manufacturer;
+       this.model = model;
+       this.yearOfService = yearOfService;
+       this.condition = condition;
+    }
 
 }
