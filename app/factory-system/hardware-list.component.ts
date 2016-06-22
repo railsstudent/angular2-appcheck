45a252b6c420d8680b08391b4fb0afb77f62267a
Connie <cleung334@gmail.com>
2016-06-22 22:40:46 +0800

    Support pagination in hardward page and visual machine page.

--- app/factory-system/hardware-list.component.ts
+++ app/factory-system/hardware-list.component.ts
@@ -1,19 +1,28 @@
 import {Component, OnInit, Input } from '@angular/core';
 import {CORE_DIRECTIVES} from '@angular/common';
 import {RouteParams} from '@angular/router-deprecated';
+import {MATERIAL_DIRECTIVES} from "ng2-material";
 
 import {HardwareListService} from './service/hardware-list.service';
 import {Hardware} from '../factory-hardware/model/hardware';
 
 @Component({
     selector: 'hardware-list',
-    templateUrl: 'app/factory-system/template/hardware-list.html'
+    templateUrl: 'app/factory-system/template/hardware-list.html',
+    directives: [MATERIAL_DIRECTIVES]
 })
 export class HardwareListComponent implements OnInit {
 
   hardwareList: Array<Hardware> = new Array<Hardware>();
   factoryCode: string;
   condition: string[];
+  pagination = {
+    currentPage: 1,
+    itemsPerPage: 5,
+    totalItems: 0
+  };
+  availableLength = [5, 10];
+  pagedHardwareList: Array<any> = [];
 
   ngOnInit() {
     console.log("ngOnInit of hardware list component fired.");
@@ -25,19 +34,29 @@ export class HardwareListComponent implements OnInit {
        this.hardwareList = [];
     }
     this.condition = this._hardwareListService.getCondition();
+    this.pagination = {
+      currentPage: 1,
+      itemsPerPage: 5,
+      totalItems: this.hardwareList.length
+    }
+    console.log(this.pagination);
+    this.refreshHardwardList();
   }
 
   constructor(private _hardwareListService: HardwareListService,
     private _routeParams: RouteParams) {
   }
 
-  @Input()
-  set refresh(refresh: boolean) {
-      console.log('HardwareListComponent: refresh = ' + refresh);
-      console.log('HardwareListComponent: factory code = ' + this.factoryCode);
-      // reload data
-      if (refresh) {
-        this.hardwareList = this._hardwareListService.getHardwareByFactory(this.factoryCode);
-      }
+  refreshHardwardList() {
+    let start = (this.pagination.currentPage - 1) * this.pagination.itemsPerPage,
+      end = start + this.pagination.itemsPerPage;
+    this.pagedHardwareList = this.hardwareList.slice(start, end);
+  }
+
+  detectChange(event) {
+    console.log('pagination detection');
+    this.pagination = event.pagination;
+    this.refreshHardwardList();
+    console.log(this.pagination);
   }
 }
--- app/factory-system/virtual-machine-list.component.ts
+++ app/factory-system/virtual-machine-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, Input } from '@angular/core';
 import {CORE_DIRECTIVES} from '@angular/common';
 import {RouteParams} from '@angular/router-deprecated';
+import {MATERIAL_DIRECTIVES} from "ng2-material";
 
 import {VirtualMachineListService} from './service/virtualmachine-list.service';
 import {VirtualMachine} from '../factory-virtualmachine/model/virtual-machine';
@@ -8,14 +9,21 @@ import {Condition} from '../factory-hardware/model/condition-enum';
 
 @Component({
     selector: 'virtual-machine-list',
-  //  directives: [MATERIAL_DIRECTIVES],
-    templateUrl: 'app/factory-system/template/virtualmachine-list.html'
+    templateUrl: 'app/factory-system/template/virtualmachine-list.html',
+    directives: [MATERIAL_DIRECTIVES]
 })
 export class VirtualMachineListComponent implements OnInit {
 
   factoryCode: string;
   vmList : Array<VirtualMachine> = new Array<VirtualMachine>();
   condition: string[];
+  pagination = {
+    currentPage: 1,
+    itemsPerPage: 5,
+    totalItems: 0
+  };
+  availableLength = [5,10];
+  pagedVMList: Array<any> = [];
 
   ngOnInit() {
     console.log("ngOnInit of virtual machine list component fired.");
@@ -26,19 +34,29 @@ export class VirtualMachineListComponent implements OnInit {
        this.vmList = [];
     }
     this.condition = this._vmListService.getCondition();
+    this.pagination = {
+      currentPage: 1,
+      itemsPerPage: 5,
+      totalItems: this.vmList.length
+    }
+    console.log(this.pagination);
+    this.refreshVMList();
   }
 
   constructor(private _vmListService : VirtualMachineListService,
     private _routeParams: RouteParams) {
   }
 
-  @Input()
-  set refresh(refresh: boolean) {
-      console.log('VirtualMachineListComponent: refresh = ' + refresh);
-      console.log('VirtualMachineListComponent: factory code = ' + this.factoryCode);
-      // reload data
-      if (refresh) {
-          this.vmList = this._vmListService.getVirtualMachineByFactory(this.factoryCode);
-      }
+  refreshVMList() {
+    let start = (this.pagination.currentPage - 1) * this.pagination.itemsPerPage,
+      end = start + this.pagination.itemsPerPage;
+    this.pagedVMList = this.vmList.slice(start, end);
+  }
+
+  detectChange(event) {
+    console.log('pagination detection');
+    this.pagination = event.pagination;
+    this.refreshVMList();
+    console.log(this.pagination);
   }
 }
