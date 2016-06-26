2b7a9218dbbd2667b280ee50cc8ea36a695d99c0
Connie Leung <cleung334@gmail.com>
2016-06-26 14:54:27 +0800

    add pagination to app dependency list
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- app/factory-app-detail/app-detail.component.ts
+++ app/factory-app-detail/app-detail.component.ts
@@ -1,18 +1,26 @@
 import {Component, OnInit } from '@angular/core';
 import {Router, RouteParams} from '@angular/router-deprecated';
 import * as _ from 'lodash';
+import {MATERIAL_DIRECTIVES} from "ng2-material";
 
 import {AppListService} from '../factory-system/service/app-list.service';
 import {AppDetail} from './model/app-detail';
 
 @Component({
   selector: 'app-detail',
-  templateUrl: 'app/factory-app-detail/template/app-detail.html' //,
-  //directives: [MATERIAL_DIRECTIVES]
+  templateUrl: 'app/factory-app-detail/template/app-detail.html',
+  directives: [MATERIAL_DIRECTIVES]
 })
 export class AppDetailComponent implements OnInit  {
 
   appDetail : AppDetail;
+  pagination = {
+    currentPage: 1,
+    itemsPerPage: 5,
+    totalItems: 0
+  };
+  availableLength = [5, 10, 15];
+  pagedAppDetailList: Array<any> = [];
 
   ngOnInit() {
     let code = this._routeParams.get('code');
@@ -23,6 +31,21 @@ export class AppDetailComponent implements OnInit  {
     } else {
         this.appDetail = null;
     }
+    if (this.appDetail) {
+      this.pagination = {
+        currentPage: 1,
+        itemsPerPage: 5,
+        totalItems: this.appDetail.dependencies.length
+      }
+    } else {
+      this.pagination = {
+        currentPage: 1,
+        itemsPerPage: 5,
+        totalItems: 0
+      }
+    }
+    console.log(this.pagination);
+    this.refreshAppDependencyList();
   }
 
   constructor(private _router: Router,
@@ -37,5 +60,22 @@ export class AppDetailComponent implements OnInit  {
        // default to TAA
        this._router.navigate(["FactorySummary", { code: 'TAA' } ]);
      }
+  }
+
+  refreshAppDependencyList() {
+    let start = (this.pagination.currentPage - 1) * this.pagination.itemsPerPage,
+      end = start + this.pagination.itemsPerPage;
+    if (this.appDetail) {
+      this.pagedAppDetailList = this.appDetail.dependencies.slice(start, end);
+    } else {
+      this.pagedAppDetailList = [];
+    }
+  }
+
+  detectChange(event) {
+    console.log('pagination detection');
+    this.pagination = event.pagination;
+    this.refreshAppDependencyList();
+    console.log(this.pagination);
   }
 }
